0c5ba67c33b246fcf6d9cc0d6913ae582522e8c3
Nicorasu <exstracrazy@gmail.com>
2018-10-24 12:48:45 +0200

    database

---
+++ WebSite1/connect.php
@@ -0,0 +1,69 @@
+﻿<?php
+// Create connection
+	$vareNavnInput = filter_input(INPUT_POST,'vareNavn');
+	$vareStrekkodeInput = filter_input(INPUT_POST,'vareStrekkode');
+	$varePrisInput = filter_input(INPUT_POST,'varePris');
+	
+	if(!empty($vareNavnInput) &&!empty($vareStrekkodeInput)&& !empty($varePrisInput)) {
+		$servername = "localhost";
+$database = "handlevognkalkulator";
+$username = "root";
+$password = "";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+		
+		if (mysqli_connect_error()){
+		  die('Connect Error ('. mysqli_connect_errno() .') '
+			. mysqli_connect_error());
+		}
+		else{
+		  $sql = "INSERT INTO kiwiprices (productname, barcode,price)
+		  values ('$vareNavnInput','$vareStrekkodeInput', 'varePrisInput')";
+		  if ($conn->query($sql)){
+			echo "New record is inserted sucessfully";
+
+		header('Location: database_View.php');
+		  }
+		  else{
+			echo "Error: ". $sql ."
+		". $conn->error;
+
+		header('Location: database_input.php');
+		  }
+		  $conn->close();
+		}
+	} else {
+		echo "everything should be filled in";
+		header('Location: index.html');
+	}
+		
+		
+	/*
+	$conn = mysqli_connect($servername, $username, $password, $database);
+
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	 
+	echo "Connected successfully";
+	
+	if(isset($_POST["submit"])){
+		$vareNavnInput = $_POST['vareNavn'];
+		$vareStrekkodeInput = $_POST['vareStrekkode'];
+		$varePrisInput = $_POST['varePris'];
+	
+
+		if($query = mysqli_query($conn,"INSERT INTO kiwiprices ('id', 'username', 'password', 'email') VALUES('', '".$username."', '".$password."', '".$email."')")){
+			echo "Success";
+			header('database_View.php'); 
+		}else{
+			echo "Failure" . mysqli_error($conn);
+			header('index.html');
+		}
+
+    }
+	*/
+?>
+
+
+
--- WebSite1/database_input.php
+++ WebSite1/database_input.php
@@ -1,37 +1,9 @@
-﻿<?php
-$servername = "localhost";
-$database = "handlevognkalkulator";
-$username = "root";
-$password = "";
-
-// Create connection
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-// Check connection
-
-if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-}
- 
-echo "Connected successfully";
- 
-$sql = "INSERT INTO kiwiprices (barcode, productcompany, producttype, productname, price) VALUES ('21234566', 'firstprice', 'vegetable',' potatoe', '18')";
-if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
-} else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-mysqli_close($conn);
-
-/*
-<!DOCTYPE html>
-<html>
+﻿<html>
 <head>
     <meta charset="utf-8" />
     <title>HK/InputDatabase</title>
     <link href="StyleSheet.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 </head>
 <body>
     <div id="nav-placeholder">
@@ -41,37 +13,12 @@ mysqli_close($conn);
             $("#nav-placeholder").load("https://raw.githubusercontent.com/h578011/Ing_Handlevogn-Kalkulatoren/master/WebSite1/nav.html");
         });
     </script>
-
     <h1>Input</h1>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <form id="myForm" action="POST" target=" no-target">
-        <input id="vareNavnInput" placeholder="Hva er varens navn?" name="vareNavn" /> <br> <br>
-        <input id="vareStrekkodeInput" placeholder="Hva er varens strekkode?" name="vareStrekkode" /> <br> <br>
-        <input id="varePrisInput" placeholder="Hva er varens pris?" name="varePris" /> <br> <br>
-        <button id="formSubmit" type="submit">Klikk her for å skrive til database</button>
+    <form method="POST" action="connect.php">
+        <input placeholder="Hva er varens navn?" name="vareNavn" /> <br> <br>
+        <input placeholder="Hva er varens strekkode?" name="vareStrekkode" /> <br> <br>
+        <input placeholder="Hva er varens pris?" name="varePris" /> <br> <br>
+        <input type="submit" value="Submit">Klikk her for å skrive til database</input>
     </form>
-
-    <iframe src="#" id="no-target" name="no-target" style="visibility:hidden"></iframe>
-    <script>
-        $('myForm').on('submit', function () {
-            var baseURL = "https://docs.google.com/forms/d/e/1FAIpQLScay6E42LjCoYzZ-4KzLp6Q0jf4GKuAfX81-ojLo1SLAo267g/formResponse?";
-            var submitRef = "&submit=M2UYVd";
-            var vareNavn = encodeURIComponent($("#vareNavnInput").val());
-            var vareNavnID = "entry.622058584";
-            var vareStrekKode = encodeURIComponent($("#vareStrekkodeInput").val());
-            var vareStrekKodeID = "entry.554778713";
-            var varePris = encodeURIComponent($("#varePrisInput").val());
-            var varePrisID = "entry.578785986";
-
-            var submitURL = (baseURL + vareNavnID + "=" + vareNavn + "&" + vareStrekKodeID + "=" + vareStrekKode + "&" + varePrisID + "=" + varePris + submitRef);
-            console.log(submitURL);
-
-            $(this)[0].action = submitURL;
-            $('#vareNavnInput').addClass('active').val("Thank Tou!");
-        });
-    </script>
 </body>
 </html>
-*/
-?>
