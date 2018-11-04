8cf0505363323a95b2e24ea17f77974cd63b2111
Nicorasu <exstracrazy@gmail.com>
2018-11-04 19:37:16 +0100

    Adding Works

--- WebSite1/database_kalkulator.php
+++ WebSite1/database_kalkulator.php
@@ -1,68 +1,145 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <title>HK/Kalkulator</title>
-    <link href="StyleSheet.css" rel="stylesheet">
-    <link rel="shortcut icon" type="image/x-icon" href="icon.ico" />
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-</head>
-<body>
-    <div id="nav-placeholder">
-    </div>
+    <head>
+        <meta charset="utf-8" />
+        <title>HK/Kalkulator</title>
+        <link href="StyleSheet.css" rel="stylesheet">
+        <link rel="shortcut icon" type="image/x-icon" href="icon.ico" />
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    </head>
 
-	<p>Under konstruksjon</p>
-    <script>
-        $(function () {
-            $("#nav-placeholder").load("https://raw.githubusercontent.com/h578011/Ing_Handlevogn-Kalkulatoren/master/WebSite1/nav.html");
-        });
-    </script>
-    <footer>&copy; 2018 Damanito</footer>
+    <body>
+        <div id="nav-placeholder">
+        </div>
 
-</body>
+        <table style ="border:1" id ="table">
+            <tr id="myRow">
+                <td>Product</td>
+                <td>Pris</td>
+            </tr>
+            <tr id="total">
+                <td>Total</td>
+                <td>0.00</td>
+            </tr>
+        </table>
+        <script>
+            $(function () {
+                $("#nav-placeholder").load("https://raw.githubusercontent.com/h578011/Ing_Handlevogn-Kalkulatoren/master/WebSite1/nav.html");
+            });
+
+            var currentValue = 0;
+
+            function setCurrentValue(arg){
+                currentValue = arg;
+            }
+
+            function addToCart(){
+                Cart.push(Products[currentValue]);
+                //console.log(Cart[Cart.length-1].aBarcode);
+                updateCart();
+            }
+
+            function updateCart(){
+                var table = document.getElementById("table");
+
+                for(var i = table.rows.length-1; i >=0 ; i--){
+                    table.deleteRow(i);
+                }
+                var head = table.insertRow(0);
+                head.insertCell(0).appendChild(document.createTextNode('Vare'));
+                head.insertCell(1).appendChild(document.createTextNode('Pris'));
+
+                var totalPrice = 0;
+                for(var i = 0; i < Cart.length ; i++){
+                    var row = table.insertRow(i + 1);
+                    row.insertCell(0).appendChild(document.createTextNode(Cart[i].aProductname));
+                    row.insertCell(1).appendChild(document.createTextNode(Cart[i].aPrice));
+                    totalPrice += Cart[i].aPrice;
+                }
+
+                var total = table.insertRow(table.rows.length);
+                total.insertCell(0).appendChild(document.createTextNode('Total'));
+                totalPrice = Math.round(totalPrice * 100) / 100;
+                total.insertCell(1).appendChild(document.createTextNode( totalPrice +'kr'));
+
+            }
+
+            var Products = new Array();
+            var Cart = new Array();
+
+            function Product (aBarcode,aProductcompany,aProducttype,aProductname,aPrice) {
+                this.aBarcode = aBarcode;
+                this.aProductcompany = aProductcompany;
+                this.aProducttype = aProducttype;
+                this.aProductname = aProductname;
+                this.aPrice = aPrice;
+            }
+        
+        </script>
+        <footer>&copy; 2018 Damanito</footer>
+
+    </body>
 </html>
 
 <?php
-$servername = "localhost";
-$database = "handlevognkalkulator";
-$username = "root";
-$password = "";
+    $file = "database.txt";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+    $barcode = 0;
+    $productcompany = 1;
+    $producttype = 2;
+    $productname = 3;
+    $price = 4;
 
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+    function readFileLine($file, $line)
+    {            
+        return explode("\n", file_get_contents($file))[$line];
+    }
+    function getFileLineCount($file)
+    {            
+        return count(explode("\n", file_get_contents($file)));
+    }
 
-$result = mysqli_query($conn,"SELECT * FROM kiwiprices");
 
+    class Product {
+        public $aBarcode = 0;
+        public $aProductcompany = 1;
+        public $aProducttype = 2;
+        public $aProductname = 3;
+        public $aPrice = 4;
 
-echo "<table border='1'>
-<tr>
-<th>barcode</th>
-<th>productcompany</th>
-<th>producttype</th>
-<th>productname</th>
-<th>price</th>
-</tr>";
+        function __construct ($aBarcode,$aProductcompany,$aProducttype,$aProductname,$aPrice ) {
+            $this->aBarcode = $aBarcode;
+            $this->aProductcompany = $aProductcompany;
+            $this->aProducttype = $aProducttype;
+            $this->aProductname =$aProductname;
+            $this->aPrice =$aPrice;
+        }
+    }
 
-if(!$result) {printf("Error: %s\n", mysqli_error($conn));
-    exit();
-} else {
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['barcode'] . "</td>";
-echo "<td>" . $row['productcompany'] . "</td>";
-echo "<td>" . $row['producttype'] . "</td>";
-echo "<td>" . $row['productname'] . "</td>";
-echo "<td>" . $row['price'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
+    $products = array();
 
-}
-mysqli_close($conn);
+    $string = '<p>
+    Add to Cart:
+    <select onChange = "setCurrentValue(this.value);">';
+
+    for($i= 0; $i < getFileLineCount($file);  $i++ )
+    {
+        $row = explode(",", readFileLine($file, $i));
+        $temp = new Product($row[0],$row[1],$row[2],$row[3],$row[4]);
+        $products[$i] = $temp;
+
+        $string = $string.'<option value="'.$i.'">'.$products[$i]->aProductname.'</option>';
+
+        
+        echo('<script> Products.push(new Product('.$row[0].',"'.$row[1].'","'.$row[2].'","'.$row[3].'",'.$row[4].')); </script>');
+    }
+
+    
+    $string = $string.'
+    </select>
+     </p>';
+
+    echo($string);
+    echo('<input  onclick = "addToCart()" value ="Add" id type="button"></button>');
+
 ?>
